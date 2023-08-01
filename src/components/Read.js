8f5cba7fe30dc47271f0f2c8beac976d6cba932d
unknown <ivicakartelo12@gmail.com>
2023-08-01 19:10:43 +0200

    Undo AI cod

--- src/components/Read.js
+++ src/components/Read.js
@@ -5,11 +5,15 @@ import axios from 'axios';
 import Update from './Update';
 import Create from './Create';
 
-
 const Read = () => {
   const [posts, setPosts] = useState([]);
+  console.log(posts)
+  const [showEditForm, setShowEditForm] = useState(false);
+  console.log(showEditForm)
+  const [updateId, setUpdateId] = useState('');
+  console.log(updateId)
   const [isLoading, setIsLoading] = useState(false);
-  const [postToUpdate, setPostToUpdate] = useState(null); // New state for the post to be updated
+  console.log(isLoading)
 
   async function fetchPosts() {
     try {
@@ -18,14 +22,13 @@ const Read = () => {
       const posts = response.data;
       setPosts(posts);
       setIsLoading(false);
+      //alert("Read.js finished rendering")
     } catch (error) {
       console.log(error);
     }
   }
 
-  useEffect(() => {
-    fetchPosts();
-  }, []);
+  useEffect(() => {fetchPosts()}, [])
 
   const handleAddNewPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -34,19 +37,21 @@ const Read = () => {
   const handleDelete = async (id) => {
     try {
       await axios.delete(`https://640114a00a2a1afebee5c77d.mockapi.io/post1/${id}`);
-      setPosts(posts.filter((post) => post.id !== id));
-    } catch (error) {
+      setPosts(posts.filter(post => post.id !== id));
+    } catch
+    (error) {
       console.log(error);
     }
   };
 
-  const handleEdit = (post) => {
-    setPostToUpdate(post); // Set the post that needs to be updated
+  const handleUpdate = (id) => {
+    setUpdateId(id);
+    setShowEditForm(true);
   };
 
   const handleUpdatePost = (updatedPost) => {
-    setPosts(posts.map((post) => (post.id === updatedPost.id ? updatedPost : post)));
-    setPostToUpdate(null); // Clear the post to update after updating it
+    setPosts(posts.map(post => post.id === updatedPost.id ? updatedPost : post));
+    setShowEditForm(false);
   };
 
   return (
@@ -54,9 +59,9 @@ const Read = () => {
       <Link to="/">Home</Link> | { }
       <Link to="/read">Admin</Link>
       <Create addNewPost={handleAddNewPost} />
-
+      
       {isLoading ? (
-        <Loader active inline="centered" />
+        <Loader active inline='centered' />
       ) : (
         <Table celled>
           <Table.Header>
@@ -69,33 +74,28 @@ const Read = () => {
           </Table.Header>
 
           <Table.Body>
-            {posts.slice().reverse().map((post) => (
-              <React.Fragment key={post.id}>
-                <Table.Row>
-                  <Table.Cell>{post.id}</Table.Cell>
-                  <Table.Cell>{post.heading}</Table.Cell>
-                  <Table.Cell>{post.blogpost}</Table.Cell>
-                  <Table.Cell>
-                    <Button color="yellow" onClick={() => handleEdit(post)}>
+            {posts.slice().reverse().map(post => ( // use slice() to create a copy of the array before reversing it
+              <Table.Row key={post.id}>
+                <Table.Cell>{post.id}</Table.Cell>
+                <Table.Cell>{post.heading}</Table.Cell>
+                <Table.Cell>{post.blogpost}</Table.Cell>
+                <Table.Cell >
+                  {showEditForm && updateId === post.id ? (
+                  <Update
+                      post={post}
+                      handleUpdatePost={handleUpdatePost}
+                      setShowEditForm={setShowEditForm}
+                    />
+                  ) : (
+                    <Button color='yellow' onClick={() => handleUpdate(post.id)}>
                       Update
                     </Button>
-                    <Button color="red" onClick={() => handleDelete(post.id)}>
-                      Delete
-                    </Button>
+                  )
+                  
+                  }
+                  <Button color='red' onClick={() => handleDelete(post.id)}>Delete</Button>
                   </Table.Cell>
-                </Table.Row>
-                {postToUpdate && postToUpdate.id === post.id && ( // Conditional rendering for the Update form
-                  <Table.Row>
-                    <Table.Cell colSpan={4}> {/* Use colSpan to span the entire row */}
-                      <Update
-                        post={postToUpdate}
-                        handleUpdatePost={handleUpdatePost}
-                        setShowEditForm={setPostToUpdate} // Pass the setPostToUpdate function to close the form
-                      />
-                    </Table.Cell>
                   </Table.Row>
-                )}
-              </React.Fragment>
             ))}
           </Table.Body>
         </Table>
@@ -103,5 +103,4 @@ const Read = () => {
     </div>
   );
 };
-
 export default Read;
